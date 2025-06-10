1875107cea4d452926418e955b964c3de3767836
Juli051 <Juli051@gmx.net>
2025-06-10 19:16:14 +0200

    some refactoring and moving files together

--- server/routes/login/loginRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import * as loginController from "../../controllers/login/loginController.js";
-
-router.post("/login", loginController.login);
-router.post("/password-forgotten", loginController.passwordForgotten);
-
-export default router;
--- server/routes/register/registerRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import * as registerController from "../../controllers/register/registerController.js";
-
-router.post("/register", registerController.register);
-router.post("/account-activation", registerController.accountActivation);
-
-export default router;
