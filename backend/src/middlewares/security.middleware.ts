389b0b1ff8e96f008513226c7b343a33148b9660
Juli051 <Juli051@gmx.net>
2025-07-12 14:22:12 +0200

    fixed cors

--- backend/src/middlewares/security.middleware.ts
+++ backend/src/middlewares/security.middleware.ts
@@ -64,7 +64,7 @@ export const setupSecurityMiddleware = (app: Express) => {
             origin(origin, callback) {
                 if (ENV.NODE_ENV !== "production") {
                     callback(null, true);
-                } else if (origin !== undefined && ENV.CORS_ALLOWED_ORIGINS.indexOf(origin) !== 1) {
+                } else if (origin !== undefined && ENV.CORS_ALLOWED_ORIGINS.includes(origin)) {
                     callback(null, true);
                 } else {
                     console.log(origin);
