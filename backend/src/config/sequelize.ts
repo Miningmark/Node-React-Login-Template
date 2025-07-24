27c33250e9e9d0ade86fd8115c4e00100d95e0af
Juli051 <Juli051@gmx.net>
2025-07-24 22:36:20 +0200

    made sequelize port optional for docker instance

--- backend/src/config/sequelize.ts
+++ backend/src/config/sequelize.ts
@@ -7,7 +7,7 @@ export const sequelize = new Sequelize({
     dialect: MariaDbDialect,
     database: ENV.DEFAULT_DATABASE_NAME,
     host: ENV.DEFAULT_DATABASE_HOST,
-    port: ENV.DEFAULT_DATABASE_PORT,
+    ...(ENV.DEFAULT_DATABASE_PORT ? { port: ENV.DEFAULT_DATABASE_PORT } : {}),
     user: ENV.DEFAULT_DATABASE_USERNAME,
     password: ENV.DEFAULT_DATABASE_PASSWORD,
     pool: {
