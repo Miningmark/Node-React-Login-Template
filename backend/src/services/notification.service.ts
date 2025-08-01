c0883b0ac17d684f37ad34180248f84a6ebf6931
Juli051 <Juli051@gmx.net>
2025-08-01 21:38:32 +0200

    added socketio for notifications

---
+++ backend/src/services/notification.service.ts
@@ -0,0 +1,29 @@
+import Notification from "@/models/notifications.model.js";
+
+export class NotificationService {
+    private routeGroupService: NotificationService;
+
+    constructor() {
+        this.routeGroupService = new NotificationService();
+    }
+
+    generateMultipleJSONResponseWithModel(databaseNotifications: Notification[]): Record<string, any> {
+        return databaseNotifications.map((databaseNotification) => {
+            return this.generateSingleJSONResponseWithModel(databaseNotification);
+        });
+    }
+
+    generateSingleJSONResponseWithModel(databaseNotification: Notification): Record<string, any> {
+        return this.generateSingleJSONResponse(databaseNotification.id, databaseNotification.name, databaseNotification.description, databaseNotification.notifyFrom, databaseNotification.notifyTo);
+    }
+
+    generateSingleJSONResponse(id: number, name: string, description: string, notifyFrom: Date, notifyTo: Date): Record<string, any> {
+        return {
+            id: id,
+            name: name,
+            description: description,
+            notifyFrom: notifyFrom,
+            notifyTo: notifyTo
+        };
+    }
+}
