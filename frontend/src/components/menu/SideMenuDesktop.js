272211b20fe6c1f856fd95c9a3841ebcf557d4ee
Markus <markus.sibbe@t-online.de>
2025-08-15 16:18:12 +0200

    chnage Mobile Menu shortcuts view

--- frontend/src/components/menu/SideMenuDesktop.js
+++ frontend/src/components/menu/SideMenuDesktop.js
@@ -119,7 +119,6 @@ export default function SideMenuDesktop({ menuFixed, setMenuFixed }) {
           <ul style={{ listStyle: "none", padding: "0 16px", margin: 0 }}>
             {menuBookmarks.map((bm, idx) => {
               const isActiveMain = bm.link === currentPath;
-              console.log("Bookmark:", bm.linkName, bm.link, isActiveMain);
               return (
                 <li key={idx} className="menu-item" style={{}}>
                   <div
